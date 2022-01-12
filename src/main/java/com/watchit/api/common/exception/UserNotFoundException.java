9c0d037a187de3b21d17e81f125082b20902426a
lafolie <antho@pcAnthony.(none)>
2022-01-12 09:15:58 +0100

    add custom exception

---
+++ src/main/java/com/watchit/api/common/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.watchit.api.common.exception;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
