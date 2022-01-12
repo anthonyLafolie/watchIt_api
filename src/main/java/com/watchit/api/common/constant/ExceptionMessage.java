a0ba957833cc7ea4e63f687300572248ec2f0fc9
lafolie <antho@pcAnthony.(none)>
2022-01-12 09:17:02 +0100

    remove unacessary message

--- src/main/java/com/watchit/api/common/constant/ExceptionMessage.java
+++ src/main/java/com/watchit/api/common/constant/ExceptionMessage.java
@@ -4,8 +4,6 @@ public final class ExceptionMessage {
     private ExceptionMessage() {
     }
 
-    public static final String CURRENT_USER_NOT_FOUND = "Current user not found";
     public static final String CURRENT_USER_CANNOT_BE_AUTHENTICATED = "Could not authenticate current user";
-    public static final String USER_ALREADY_EXISTS = "User account already exists";
     public static final String PSEUDO_EMAIL_TAKEN = "Pseudo or email already taken";
 }
