f19ace601de310fe6cc9658981c1224f44037da6
AnthonyLafolie <anthony59930@outlook.fr>
2022-01-13 05:43:49 +0100

    add equals method for test

--- src/main/java/com/watchit/api/dto/authentification/CredentialDto.java
+++ src/main/java/com/watchit/api/dto/authentification/CredentialDto.java
@@ -8,4 +8,15 @@ import lombok.Setter;
 public class CredentialDto {
     private String username;
     private String password;
+
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if ((obj == null) || ! this.getClass().equals(obj.getClass()))
+            return false;
+        CredentialDto other = (CredentialDto) obj;
+        if (username.equals(other.getUsername()) && password.equals(other.getPassword()))
+            return true;
+        return false;
+    }
 }
