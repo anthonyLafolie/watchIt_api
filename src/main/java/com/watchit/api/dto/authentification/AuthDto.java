dfef500fcbd6040daab2bcd849fe4e06d29f9dd4
AnthonyLafolie <anthony59930@outlook.fr>
2022-01-13 06:40:36 +0100

    add equals method for test

--- src/main/java/com/watchit/api/dto/authentification/AuthDto.java
+++ src/main/java/com/watchit/api/dto/authentification/AuthDto.java
@@ -8,4 +8,15 @@ import lombok.Setter;
 public class AuthDto {
     private String token;
     private Long userId;
+
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if ((obj == null) || ! this.getClass().equals(obj.getClass()))
+            return false;
+        AuthDto other = (AuthDto) obj;
+        if (token.equals(other.getToken()) && userId.equals(other.getUserId()))
+            return true;
+        return false;
+    }
 }
