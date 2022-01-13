f81c092e171bd766f3ca0e92d97d708ec5477154
AnthonyLafolie <anthony59930@outlook.fr>
2022-01-13 11:05:54 +0100

    add equals method for test

--- src/main/java/com/watchit/api/dto/user/UserDto.java
+++ src/main/java/com/watchit/api/dto/user/UserDto.java
@@ -13,4 +13,15 @@ public class UserDto implements UserBaseDto {
     private String email;
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
+
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if ((obj == null) || ! this.getClass().equals(obj.getClass()))
+            return false;
+            UserDto other = (UserDto) obj;
+        if (username.equals(other.getUsername()) && email.equals(other.getEmail()) && password.equals(other.getPassword()))
+            return true;
+        return false;
+    }
 }
