15c09235ed91694423abb34a5a210fba2192c550
AnthonyLafolie <anthony59930@outlook.fr>
2022-01-25 07:28:50 +0100

    create route to get current user

--- src/main/java/com/watchit/api/controller/UserController.java
+++ src/main/java/com/watchit/api/controller/UserController.java
@@ -1,13 +1,24 @@
 package com.watchit.api.controller;
 
+import com.watchit.api.dto.user.UserDto;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @CrossOrigin
 @RequestMapping("/user")
 public interface UserController {
+    /***
+     * get current user
+     *
+     * @return ResponseEntity<UserDto> user information
+     */
+    @GetMapping("/me")
+    ResponseEntity<UserDto> getCurrentUser();
+
     /***
      * delete current user
      *
--- src/main/java/com/watchit/api/controller/impl/UserControllerImpl.java
+++ src/main/java/com/watchit/api/controller/impl/UserControllerImpl.java
@@ -2,16 +2,29 @@ package com.watchit.api.controller.impl;
 
 import com.watchit.api.common.exception.CurrentUserAuthorizationException;
 import com.watchit.api.controller.UserController;
+import com.watchit.api.dto.user.UserDto;
 import com.watchit.api.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
-public class UserControllerImpl implements  UserController{
+@RestController
+public class UserControllerImpl implements UserController {
 
     @Autowired
     UserService userService;
+
+    @Override
+     public ResponseEntity<UserDto> getCurrentUser() {
+        try {
+            UserDto userDto = userService.getCurrentUserDto();
+            return new ResponseEntity<>(userDto, HttpStatus.OK);
+        } catch (CurrentUserAuthorizationException unfe) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+    }
 
     @Override
     public ResponseEntity<String> deleteCurrentUser() {
@@ -22,5 +35,5 @@ public class UserControllerImpl implements  UserController{
         }
         return new ResponseEntity<>("Current user deleted", HttpStatus.OK);
     }
-    
+
 }
