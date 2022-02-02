7cefd980b9dea00ffff815bd33bc05aa13cc0d36
AnthonyLafolie <anthony59930@outlook.fr>
2022-02-02 22:26:54 +0100

    add test for getFilters

--- src/main/java/com/watchit/api/dto/filter/FilterDto.java
+++ src/main/java/com/watchit/api/dto/filter/FilterDto.java
@@ -8,4 +8,15 @@ import lombok.Setter;
 public class FilterDto {
     String name;
     boolean checked;
+
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if ((obj == null) || !this.getClass().equals(obj.getClass()))
+            return false;
+        FilterDto other = (FilterDto) obj;
+        if (name.equals(other.getName()) && checked == other.isChecked())
+            return true;
+        return false;
+    }
 }
